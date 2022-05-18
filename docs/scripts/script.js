0763699bda82ab22f46a66e893f83fdd12a2dc61
Yanqiao Chen <chen02@allegheny.edu>
2022-05-18 17:19:24 -0400

    Update

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,9 +1,12 @@
-var randomCards = []
+var deck = Array.from({ length: 22 }, (_, i) => i)
 var userChoices = []
-var pstionId = ["1","2","3","4","5","6","7","8","9"]
+var uniqueDraws = []
+var positions = ["1","2","3","4","5","6","7","8","9"]
 // (image, position) pairs
 var cardPstionPair = []
+var tracker = 4
 var chance = 8;
+var time = 40
 var startBtn = document.getElementById("startBtn");
 var gameBtn = document.querySelectorAll(".gameButton")
 
@@ -19,7 +22,7 @@ gameBtn.forEach(
                         compare()
                     }
                 },500)
-
+                chanceCheck()
             }
         )
     }
@@ -27,46 +30,73 @@ gameBtn.forEach(
 startBtn.addEventListener(
     "click", ()=>{
         setUp()
-        playTurn()
         elem = document.getElementById("Instruction")
         elem.setAttribute("hidden","")
+        const countDown = setInterval (()=>{
+            time --
+            document.getElementById("time").innerText=`Time left: ${time}`
+            if (tracker <= 1){
+                victory()
+                clearInterval(countDown)
+            }
+            if(time <= 0){
+                defeated()
+                clearInterval(countDown)
+            } },1000)
+            
     }
+    
 )
 function setUp(){    
-    for(let step = 0; step < 5; step ++ ){
-        let chosenIndex = Math.floor(Math.random()*22)
-        while (randomCards.includes(chosenIndex.toString())){
-            chosenIndex = Math.floor(Math.random()*22)
-        }
-        randomCards.push(chosenIndex.toString())
-    }
-    console.log(randomCards)
-    setPair(randomCards)
-    drawImag(cardPstionPair)
+    positions = shuffle(positions);
+    pairs()
+    drawImag()
     
 }
-function setPair(targets){
-    // Shuffle positionId
-    pstionId = shuffle(pstionId)
-    for (let step = 0; step < 5; step ++)
-    {    
-        if(pstionId.length > 1){
-            cardPstionPair.push([targets[step],pstionId.pop()])
-            cardPstionPair.push([targets[step],pstionId.pop()])
+const chooseIndex = () => {
+    let index = Math.floor(Math.random() * deck.length);
+    deck.splice(index, 1);
+    return index.toString();
+  }
+  
+  // Draw; we agree that index 0-3 are pairs, 4 is single
+  function pairs() {
+    let i = 0;
+    // Pairs
+    while(i < 4) {
+      uniqueDraws.push(chooseIndex());
+      i++;
     }
-    else{
-        cardPstionPair.push([targets[step],pstionId.pop()])
-    };
-    console.log(cardPstionPair)
-}
-}
-function drawImag(Pairs){
-    for (let step = 0; step < 9; step ++){
-        let elem = document.getElementById(Pairs[step][1])
+    console.log(deck)
+    uniqueDraws = uniqueDraws.concat(uniqueDraws);
+    // Single
+    uniqueDraws.push(chooseIndex());
+    uniqueDraws = shuffle(uniqueDraws);
+    console.log(uniqueDraws)
+  }
+// function setPair(targets){
+//     // Shuffle positionId
+//     pstionId = shuffle(pstionId)
+//     for (let step = 0; step < 5; step ++)
+//     {    
+//         if(pstionId.length > 1){
+//             cardPstionPair.push([targets[step],pstionId.pop()])
+//             cardPstionPair.push([targets[step],pstionId.pop()])
+//     }
+//     else{
+//         cardPstionPair.push([targets[step],pstionId.pop()])
+//     };
+//     console.log(cardPstionPair)
+// }
+// }
+function drawImag(){
+    for ( let i =0; i < uniqueDraws.length; i++){
+        console.log(typeof(uniqueDraws[i]))
+        let elem = document.getElementById(positions[i])
         let imag = document.createElement("img")
-        imag.setAttribute("src",`image/${Pairs[step][0]}.jpg`)
-        imag.setAttribute("alt",`image/${Pairs[step][0]}.jpg`)
-        imag.setAttribute("id",`${Pairs[step][0]}`)
+        imag.setAttribute("src",`image/${uniqueDraws[i]}.jpg`)
+        imag.setAttribute("alt",`tarot`)
+        imag.setAttribute("id",`${uniqueDraws[i]}`)
         imag.setAttribute("style","position:relative ;height: 20vh; width: 15vh; left:-1vh;")
         // imag.setAttribute("hidden","true")
         elem.append(imag)
@@ -82,9 +112,6 @@ function activateButton(buttonId){
     userChoices.push([imagId,buttonId])
 }
 
-function playTurn(){
-
-}
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -114,6 +141,38 @@ function compare (){
         imagTwo[0].setAttribute("hidden","true")
 
     }
+    else if(userChoices[0][0] == userChoices[1][0]&&
+        userChoices[0][1] != userChoices[1][1]
+        ){
+        tracker --
+    }
     userChoices = []
     chance --
+    document.getElementById("chance").innerText = `Chance left: ${chance}`
 }
+
+function chanceCheck(){
+ if(chance < 1){
+defeated()
+ }
+}
+
+function defeated(){
+    gameBtn.forEach(
+        elem =>{
+            elem.setAttribute("disabled","true")
+        }
+    )
+    document.getElementById("defeated").removeAttribute("hidden")
+}
+
+function victory(){
+    gameBtn.forEach(
+        elem =>{
+            elem.setAttribute("disabled","true")
+        }
+    )
+    document.getElementById("victory").removeAttribute("hidden")
+}
+
+
